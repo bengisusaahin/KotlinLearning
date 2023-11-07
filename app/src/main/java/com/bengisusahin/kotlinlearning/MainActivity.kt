9102d56504f6dae44dcf17c280f3802499a45424
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-07 23:47:24 +0300

    Variables

--- app/src/main/java/com/bengisusahin/kotlinlearning/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/kotlinlearning/MainActivity.kt
@@ -7,5 +7,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // variables && constants
+        var x = 5
+        var y = 4
+
+        println(x*y)
+        var age = 35
+        println(age / 7*5)
+        age = 36
+        println(age)
+        val name = "James"
+        val a = 5
+
+
     }
 }
